bf9a57513f5d871c41467d0c7d325baf4cbe5dd8
Gevin Madharha <gevinmadharha@gmail.com>
2020-04-11 16:04:29 -0400

    working version of the code

--- covid.py
+++ covid.py
@@ -11,24 +11,35 @@ table = soup.find('table',id="main_table_countries_today")
 table_header = table.find('thead').find_all('th')
 
 
-with open('results.csv','w') as f:
+with open('results.csv','w',newline='') as f:
 
     writer = csv.writer(f)
 
     header = []
+    new_list = []
 
     for th in table_header:
         header.append(th.text)
 
-    header.replace('\n')    
+    for element in header:
+        new_list.append(element.strip())
 
-    print(header)
+    # remove \xa0 from the word
+    new_list[8] = new_list[8].replace(u'\xa0', ' ')
 
+    # remove \n from the word
+    new_list[11] = new_list[11].replace('\n',' ')
+
+    writer.writerow(new_list)
 
     for row in table.find_all('tr'):
         csvRow = []
         for data in row.find_all('td'):
             csvRow.append(data.text)
+        
+        for i in range(len(csvRow)):
+            csvRow[i] = csvRow[i].replace('\n','')
+
         writer.writerow(csvRow)
 
 f.close()
