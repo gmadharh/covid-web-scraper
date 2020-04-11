149d55e646d6dc58fe544f9cb268624120256086
Gevin Madharha <gevinmadharha@gmail.com>
2020-04-11 15:19:59 -0400

    working on writing the data to a csv

--- covid.py
+++ covid.py
@@ -1,1 +1,34 @@
-print('hello world')
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+source = requests.get('https://www.worldometers.info/coronavirus/').text
+
+soup = BeautifulSoup(source,'lxml')
+
+table = soup.find('table',id="main_table_countries_today")
+
+table_header = table.find('thead').find_all('th')
+
+
+with open('results.csv','w') as f:
+
+    writer = csv.writer(f)
+
+    header = []
+
+    for th in table_header:
+        header.append(th.text)
+
+    header.replace('\n')    
+
+    print(header)
+
+
+    for row in table.find_all('tr'):
+        csvRow = []
+        for data in row.find_all('td'):
+            csvRow.append(data.text)
+        writer.writerow(csvRow)
+
+f.close()
