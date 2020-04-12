f19486a8e49095611cc2bc8a03a5fe3e22476bf0
Gevin Madharha <gevinmadharha@gmail.com>
2020-04-12 11:38:17 -0400

    added comments

--- covid.py
+++ covid.py
@@ -2,25 +2,38 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
-source = requests.get('https://www.worldometers.info/coronavirus/').text
+# url
+URL = 'https://www.worldometers.info/coronavirus/'
 
+# use requests to get the html at the URL
+source = requests.get(URL).text
+
+# BeautifulSoup object on the html
 soup = BeautifulSoup(source,'lxml')
 
+# get the table with all of the countries/stats
 table = soup.find('table',id="main_table_countries_today")
 
+# get the table header
 table_header = table.find('thead').find_all('th')
 
-
+# open a csv file to write the results to
 with open('results.csv','w',newline='') as f:
 
+    # csv writer object
     writer = csv.writer(f)
 
+    # two lists used to get the header
+    # header - the raw data from the header
+    # new list - the data after parsing it to remove newlines / other unicode characters
     header = []
     new_list = []
 
+    # get the data from the header and add it to the list
     for th in table_header:
         header.append(th.text)
 
+    # strip the new lines and add it to the new list
     for element in header:
         new_list.append(element.strip())
 
@@ -30,16 +43,27 @@ with open('results.csv','w',newline='') as f:
     # remove \n from the word
     new_list[11] = new_list[11].replace('\n',' ')
 
+    # write the row to the csv
+    # header row
     writer.writerow(new_list)
 
+    # go through each row in the table
     for row in table.find_all('tr'):
+
+        # csvRow list used to hold the data in each row
+        # empty after the data is written to the csv
         csvRow = []
+
+        # get the data and add it to the list
         for data in row.find_all('td'):
             csvRow.append(data.text)
         
+        # remove all \n from the data
         for i in range(len(csvRow)):
             csvRow[i] = csvRow[i].replace('\n','')
 
+        # write the data to the csv
         writer.writerow(csvRow)
 
+# close file
 f.close()
